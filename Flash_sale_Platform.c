c4b85bdfb1da29dcea703c831ca92579105cafb6
WANASINGHE MUDIYANSELAGE PAMODHA VIKUM WANASINGHE <251212921+ict2024130-ux@users.noreply.github.com>
2026-05-16 12:16:14 +0530

    admin

--- Flash_sale_Platform.c
+++ Flash_sale_Platform.c
@@ -3,7 +3,27 @@
 #include<string.h>
 #include<time.h>
 
+/* ================================================Data base===================================================*/
+struct events
+{
+  char Event_Name[100]; /*That show s the all events names */
+  char date[11];/*that shows created date*/
+  int units;/*that also show the total units of having event*/
+    /* data */
+};
+
+
+/*=============================================================================================================*/
+void Event_Managemnt(){}
+
+
 int main(){
 
+/* ----------------------------------------------------------------------------------------------*/
+    printf("SAMPLE DATA BASE updating......\n");
+
+    
+/*-----------------------------------------------------------------------------------------------*/
+    Event_Managemnt();
     return 0;
 }
